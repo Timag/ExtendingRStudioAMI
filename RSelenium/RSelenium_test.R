aecea0bdde9fa27c0480560eb1467e6b718ba644
Tonio Liebrand <andreas.liebrand@hof.uni-frankfurt.de>
2019-11-01 21:50:46 +0100

    Add files via upload

---
+++ RSelenium/RSelenium_test.R
@@ -0,0 +1,16 @@
+library(RSelenium)
+
+portNr <- 12432
+system(paste0("sudo -kS docker run -d -p ", portNr,
+ ":4444 selenium/standalone-firefox:2.53.0"),
+       input = "INSERTPASSWORDPLEASEHASH")
+
+remDr <- remoteDriver(port = portNr)
+
+remDr$open()
+
+remDr$navigate(url = "https://www.r-bloggers.com")
+
+remDr$screenshot(display = TRUE)
+
+remDr$close()
